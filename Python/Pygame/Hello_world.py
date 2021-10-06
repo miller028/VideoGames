f3e862f56edf48218201e287f00daaae5ce82e5a
“your <millersalas028@gmail.com>
2021-10-05 21:32:51 -0500

    changes

--- Python/Pygame/Hello_world.py
+++ Python/Pygame/Hello_world.py
@@ -15,8 +15,28 @@ pygame.init()
 #Dimenzionar el tamaño (w x h) de la ventana del juego 
 width = 800
 height = 500
-windows = pygame.display.set_mode((width, height))
+mywindows = pygame.display.set_mode((width, height))
 pygame.display.set_caption("¡Hello world!")
+
+'''
+   RGB:
+   Colores Red, Green, Blue de fondo, trabajados en Hexadecimal que desde
+   0 hasta 255
+'''
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+x = pygame.Color(210, 180, 140)
+y = pygame.Color(70, 130, 180)
+
+#Poner figura (rectangulo, circulo, cuadrado, etc)
+rect1 = pygame.Rect(150, 300, 150, 50) #Rectangulo en x, y, w, h
+rect2 = pygame.Rect(350, 230, 150, 50) #Rectangulo en x, y, w, h
+print(rect2,x)
+print(rect2,y)
+
+rect1.center = (width // 2, height // 2)#Punto medio de la ventana 
 
 #Metodo para mantener visible la ventana abierta
 while True:
@@ -24,5 +44,15 @@ while True:
         if event.type == pygame.QUIT : #si preciona x en la ventana sale
             pygame.QUIT #cierra la ventana
             sys.exit()#cierra o destruye todos los procesos 
+    mywindows.fill(y)
+    pygame.draw.rect(mywindows, red, rect1)#Para dibujar se nesecita el contexto, color, rectangulo
+    pygame.draw.rect(mywindows, blue, rect2)
+    
+    pygame.draw.rect(mywindows, green, (50, 50, 50, 50)) 
+    pygame.draw.line(mywindows, red, (10,10), (300,10),5)
+    pygame.draw.circle(mywindows, x, (400,200), 100)
+    pygame.draw.polygon(mywindows, blue, ((0,300), (100,200), (200,300)))
+    pygame.display.update() #para actualizarse
+
 
 
