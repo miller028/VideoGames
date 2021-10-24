c429dbfb270dc9ee086a06bd1e988239e3e3a08a
“your <millersalas028@gmail.com>
2021-10-23 21:31:39 -0500

    changes

--- Python/Pygame/atari.py
+++ Python/Pygame/atari.py
@@ -8,6 +8,7 @@
 #Importamos librerias ((Inicio):
 import sys
 import pygame
+import time
 #Importamos librerias (Fin):
 
 #Inicializar Pygame:
@@ -89,6 +90,20 @@ class Wall(pygame.sprite.Group):#
     
 #Fin de Clase muro 
 
+#Inicio de Funcion o medoto  Game_Over
+def Game_Over ():#Metodo o funcion si llega a tocar piso, pierda }
+     mensaje = 'Perdiste, Vuelve a intentarlo'
+     TextoColor = (0, 0, 139)#color del texto en RGB
+     TextoEstilo = pygame.font.SysFont ('Arial', 30)#El tipo de fuente es de la libreria pygame
+     txt_windows = TextoEstilo.render (mensaje, True, TextoColor)#Primer parametro → mensaje. Segundo parametro → True. Tercer parametro → color
+     txt_windows_rect = txt_windows.get_rect ()#txt_windows_rect se guarda el rectangulo que se genera de txt_windows
+     txt_windows_rect.center = [WIDTH/2, HEIGHT/2]#Se divide entre 2, para centrarlo en pantalla
+     mywindows.bit (txt_windows, txt_windows_rect)#Setee texto en la pantalla principal. Llevara 2 parametros: parametro 1 → texto del render, parametro 2 → variable del rectangulo 
+     pygame.display.fip ()
+     time.sleep (2)#Pausa de 2 segundos para mostrar el mensaje 
+
+#Fin de Funcion o medoto  Game_Over
+
 #Dimenzionar el tamaño (w x h) de la ventana del juego 
 WIDTH = 640
 HEIGHT = 480
@@ -115,8 +130,8 @@ BG_COLOR = (0, 191, 255) # (Red, Green, Blue)
 ball = Ball()#se llama la instancia de la clase bolita (Ball) y nombra una variable(ball)
 player = Bar()# manipula la barra el jugador
 
-total_latrillos = int(input("Digita la cantidad de ladrillos a generar "))
-wall = Wall(total_latrillos)#muro que tiene 112 ladrillos
+#total_latrillos = int(input("Digita la cantidad de ladrillos a generar "))
+wall = Wall(150)#muro que tiene 112 ladrillos
 #Fin de invocaciones
 
 #Loop (Revisión cíclica de los eventos) => Listener esta pendiente del acontecimiento
@@ -160,5 +175,9 @@ while True:
             ball.speed [1] = ball.speed [1]#Vamos afectar la trayectoria
               
         wall.remove(Brick)#aqui se quitan los ladrillos
+
+    if ball.rect.bottom >= HEIGHT:#llamar la funcion Game_Over solo cuando la bola toca piso
+      #  Game_Over ()#LLamamos a la funcion Game_Over, cuando la pelota toque piso
+
 
 #Fin de programacion de Collisions entre la bola y el wall(ladrillos)
